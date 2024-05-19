1529f06d9f7d0642cfebaa0342e36e21ee859561
silas <silasmaques@outlook.com>
2024-05-18 21:18:55 -0300

    teste

--- Anotacoes de Aulas/2024.1/ESTRUTURA DE DADOS EM C/PROJETOS/projeto-01/soma.h
+++
@@ -1,3 +0,0 @@
-int somar(int n1, int n2){
-    return n1 + n2;
-}
--- Anotacoes de Aulas/2024.1/ESTRUTURA DE DADOS EM C/atividades/Atividade-em-C/main.c
+++
@@ -1,9 +0,0 @@
-#include "MinhasLib/questionario.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(void) {
-  Q10();
-  system("Pause");
-  return 0;
-}
